f67b645291653fd4ddb3df5e4f76990241d0bef7
backcoder <backcoder2@gmail.com>
2022-09-27 00:08:29 +0900

    oneProductCSS

--- src/main/java/connectus/uploadConfig.java
+++ src/main/java/connectus/uploadConfig.java
@@ -10,6 +10,7 @@ public class uploadConfig implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/upload/**").addResourceLocations("file:/Users/youngban/upload/");
+		registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
 		
 	}
 	
